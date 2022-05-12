3cc32d87a4aed9dba6898ac9b60d78ab981e4d8d
Kunal Pal <52573140+silentwraith03@users.noreply.github.com>
2022-05-12 09:56:26 +0530

    Add files via upload

---
+++ greatest_of_3.php
@@ -0,0 +1,28 @@
+<?php
+$a = 10;
+$b = 20;
+$c = 30;
+
+if($a>$c)
+{
+    if($a>$c)
+    {
+        echo "$a is greatest";
+    }
+    else
+    {
+        echo "$c is greatest";
+    }
+    
+}
+else{
+    if($b>$c)
+    {
+        echo "$b is greatest";
+    }
+    else
+    {
+        echo "$c is greatest";
+    }
+}
+?>
